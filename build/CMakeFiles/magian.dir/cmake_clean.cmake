fdf42d3357aa6a0e1098175ec63d4cf994920962
Sumeet Singh <sumeet.singhji@outlook.com>
2023-06-05 18:25:17 +1000

    v0.2.2

---
+++ build/CMakeFiles/magian.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/magian.dir/staging.cpp.obj"
+  "CMakeFiles/magian.dir/staging.cpp.obj.d"
+  "libmagian.dll.a"
+  "magian.exe"
+  "magian.exe.manifest"
+  "magian.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/magian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
